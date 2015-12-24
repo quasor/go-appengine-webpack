55b89c2f6c0cc1bc61f14c4997b425a4b775276c
Arran Ubels <arran4@gmail.com>
2015-12-25 00:55:53 +1100

    Minor improvements.

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-  _ "api"
-  "math/rand"
-  "time"
+	_ "api"
+	"math/rand"
+	"time"
 )
 
 func init() {
-  rand.Seed(time.Now().UTC().UnixNano())
+	rand.Seed(time.Now().UTC().UnixNano())
 }
